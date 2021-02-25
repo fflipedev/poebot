f6ec5454c2dd6cf0a4ff1be759c289869427d74d
ffelipedev <felipe.hrtz2@gmail.com>
2021-02-25 01:23:03 -0300

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const prefixo = '!';
 
-client.on('ready', function (message) {
+client.on('ready', function ( ) {
     // client.channels.cache.get('795676425852026900').send('eae');
 
 });
@@ -24,32 +24,12 @@ client.on("message", function (message) {
     }
 
     if(command === "search")  {
-        
-        var options = {
-            method: 'POST',
-            url: 'https://www.pathofexile.com/api/trade/search/HARDCORE%20RITUAL',
-            headers: {
-              cookie: '__cfduid=d4d17b789b3be3d107fb5576a889320fd1614116587; POESESSID=faf4e8239c24b2db3639a59ebf027ca6',
-              'Content-Type': 'application/json'
-            },
-            data: {
-              query: {
-                status: {option: 'online'},
-                name: args,
-                type: 'Body',
-                stats: [{type: 'and', filters: []}]
-              },
-              sort: {price: 'asc'}
-            }
-          };
-          
-          axios.request(options).then(function (response) {
-            message.reply(JSON.stringify(response.data))
-          }).catch(function (error) {
-            if(error.response) {
-                message.reply(JSON.stringify(error.response.data))
-            }
-          });
+       
+      
+    }
+
+    if(command === 'outrocomando') {
+
     }
 });
 
